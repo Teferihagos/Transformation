f00f4175563031e8a50cef7fb771f0f6fb8be7c6
Consultant <bariafl@gmail.com>
2023-12-28 12:05:33 -0500

    Py test.

---
+++ Test.py
@@ -0,0 +1,39 @@
+# name = input("Enter your name:")
+# print(f"Shalom, {name}!")
+
+# name = "Falom"
+# age = 10
+# print("Your name is {}. You are {} years old.").format(name,age)
+
+# num1 = 5
+# num2 = 20
+# print(f"Five plus twenty is {num1 + num2}")
+appName = "PythonZ"
+print(appName[:4])
+print(appName[2:5])
+print(appName[3:])
+print(appName[::2])
+
+fname = "falom"
+lname = "Teferi"
+print(" ".join([fname, lname]))
+
+print(fname.upper())
+
+str = "Python"
+print(str.islower())
+print(str.upper())
+print(str.lower())
+print(str.replace("P","A"))
+
+message = "Welcome to Heavenly"
+print(message[3::3])
+
+print(list(range(5)))
+print(list(range(1,5)))
+
+print(list(range(1,10,2)))
+
+for falom in range(5):
+    print("Falom")
+
